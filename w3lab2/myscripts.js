6560bc5a3b07ea97d3912d68805138b6baef2d00
rkamou <51096235+rkamou@users.noreply.github.com>
2019-08-07 00:17:17 -0500

    Update myscripts.js

--- w3lab2/myscripts.js
+++ w3lab2/myscripts.js
@@ -1,45 +1,60 @@
  $(function(){
  	console.log("Window loading over");
  	 $("#start").click(start_onclick);
- 	 let z = $(".item4")[0];
- 	 $(z).width("100%");
- 	  
  });
 var timer = null;
 var max_width;
 function start_onclick(){
-		
+	let nbCir = $("#nbCir").val(); 
+	
 		let z = $(".item4")[0];
+		$(z).empty();
  	 	$(z).width("100%");
  	 	max_width = $(z).width();
+ 	 	let x = $("#init_width").val();
 
- 		let x = $("#init_width").val();
- 		$("#circle").show();
- 		$("#circle").width(x+"px");
- 		$("#circle").height(x+"px");
- 		if (timer === null) {
-			timer = setInterval(animation_circle, $("#growRate").val());
-		}
+ 	 	for (let i = 1; i <= nbCir; i++) {
+			elemt = element(x,i*10); 
+			$(z).append(elemt);
+			setInterval(animation_circle, $("#growRate").val(),elemt,new Number($("#growthAmount").val()));
+		};	
+
 		console.log("Initialization complited. width : "+ x );
 }
 
-function animation_circle(){
-	console.log("Animation : "+$("#circle").width());
-	let x = $("#circle").width();
+function animation_circle(elemt, GrA){
+	let x = $(elemt).width();
 	let y = $(".item4")[0];
-	if (x>max_width-150) {
+	if (x>max_width-50) {
 		clearInterval(timer);
 		timer = null;
+		console.log("End Animation");
 	}else{
-		console.log("X= "+x);
-		console.log("#growthAmount= "+$("#growthAmount").val());
-		let z = new Number(x)+ new Number($("#growthAmount").val());
-		$("#circle").width(z);
-		$("#circle").height(z);
+		let z = new Number(x)+ GrA;
+		$(elemt).width(z);
+		$(elemt).height(z);
 	}
 }
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+function element(x,i){
+	let elemt = $("<div>");
+	elemt.addClass("circle");
+	elemt.width(x); elemt.height(x);
+	elemt.css({"background-color":getRandomColor()});
+	elemt.css({"z-index":i});
+	elemt.css({"right":(i-1)*4});
+	elemt.show();
+	return elemt;
+} 
 
 
 
